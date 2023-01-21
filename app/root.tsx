2aea7798fffcd5bd4b2cf1ae127a346f8d007b1e
Ioannis Potouridis <potouridisio@gmail.com>
2023-01-21 08:44:12 +0200

    chore: import the compiled CSS file

--- app/root.tsx
+++ app/root.tsx
@@ -1,11 +1,17 @@
 import type { LinksFunction, MetaFunction } from '@remix-run/cloudflare';
 import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
 
+import styles from './styles/app.css';
+
 export const links: LinksFunction = () => [
   {
     href: '/favicon.svg',
     rel: 'icon',
     type: 'image/svg+xml',
+  },
+  {
+    href: styles,
+    rel: 'stylesheet',
   },
 ];
 

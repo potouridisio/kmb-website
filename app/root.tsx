b404db72fa56418635e648a1ae811fb932627202
Ioannis Potouridis <potouridisio@gmail.com>
2023-01-21 08:29:15 +0200

    chore: initial commit

---
+++ app/root.tsx
@@ -0,0 +1,35 @@
+import type { LinksFunction, MetaFunction } from '@remix-run/cloudflare';
+import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
+
+export const links: LinksFunction = () => [
+  {
+    href: '/favicon.svg',
+    rel: 'icon',
+    type: 'image/svg+xml',
+  },
+];
+
+export const meta: MetaFunction = () => ({
+  charset: 'utf-8',
+  title: 'KMB',
+  viewport: 'width=device-width,initial-scale=1',
+});
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <main>
+          <Outlet />
+        </main>
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}

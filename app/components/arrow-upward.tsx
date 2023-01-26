985cc2ee37d5d52150dcc9ac887ae39e3f08270d
Ioannis Potouridis <potouridisio@gmail.com>
2023-01-26 05:07:40 +0200

    feat: add the ArrowUpward component

---
+++ app/components/arrow-upward.tsx
@@ -0,0 +1,14 @@
+import type { SVGProps } from 'react';
+
+export default function ArrowUpward(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg fill="none" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <path
+        clipRule="evenodd"
+        d="M0.330102 11.9386L10.2383 2.03039L10.2382 2.03023L11.7625 0.505859L11.7627 0.506025L11.7629 0.505859L13.2872 2.03023L13.2871 2.03039L23.1953 11.9386L21.6709 13.463L12.8405 4.63253V23.2562H10.6847V4.63278L1.85447 13.463L0.330102 11.9386Z"
+        fill="currentColor"
+        fillRule="evenodd"
+      />
+    </svg>
+  );
+}

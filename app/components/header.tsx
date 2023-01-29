545b4e95fa6c85486a0bc1190e2685d133444534
Ioannis Potouridis <potouridisio@gmail.com>
2023-01-29 18:11:19 +0200

    feat: initial commit for the Index route

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -2,6 +2,8 @@ import { gsap } from 'gsap-trial';
 import ScrollTrigger from 'gsap-trial/ScrollTrigger';
 import { useIsomorphicLayoutEffect } from '~/utils/use-isomorphic-layout-effect';
 
+import { Link } from '@remix-run/react';
+
 export default function Header() {
   useIsomorphicLayoutEffect(() => {
     const showAnim = gsap
@@ -24,11 +26,13 @@ export default function Header() {
   return (
     <header className="fixed top-1.5 z-40 w-full bg-white bg-opacity-10 py-6 backdrop-blur-lg">
       <div className="px-6 xl:px-16">
-        <div className="grid h-9 w-9 grid-cols-2 gap-1">
-          <span className="col-start-2 h-4 w-4 bg-purple [clip-path:circle(50%_at_50%_50%)]" />
-          <span className="h-4 w-4 bg-purple [clip-path:polygon(30%_0%,_70%_0%,_100%_30%,_100%_70%,_70%_100%,_30%_100%,_0%_70%,_0%_30%)]" />
-          <span className="h-4 w-4 bg-purple [clip-path:inset(0_0_0_0)]" />
-        </div>
+        <Link to="/">
+          <div className="grid h-9 w-9 grid-cols-2 gap-1">
+            <span className="col-start-2 h-4 w-4 bg-purple [clip-path:circle(50%_at_50%_50%)]" />
+            <span className="h-4 w-4 bg-purple [clip-path:polygon(30%_0%,_70%_0%,_100%_30%,_100%_70%,_70%_100%,_30%_100%,_0%_70%,_0%_30%)]" />
+            <span className="h-4 w-4 bg-purple [clip-path:inset(0_0_0_0)]" />
+          </div>
+        </Link>
       </div>
     </header>
   );
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,3 +1,31 @@
+import ArrowUpward from '~/components/arrow-upward';
+import ButtonWithIcon from '~/components/button-with-icon';
+
+const icon = <ArrowUpward className="h-[1em] w-[1em] rotate-45 text-[1rem] xl:text-[1.375rem] 3xl:text-[1.75rem]" />;
+
 export default function Index() {
-  return <div />;
+  return (
+    <div className="bg-white text-black">
+      <section className="flex min-h-screen items-center p-6 xl:p-16">
+        <div className="xl:grid xl:grid-cols-12 xl:gap-x-16">
+          <div className="max-xl:mb-16 xl:col-span-7">
+            <h1 className="text-header">Dedicated Software Boutique.</h1>
+          </div>
+          <div className="xl:col-span-5 xl:flex xl:flex-col xl:items-start xl:justify-end">
+            <p className="text-body mb-16">
+              We scale through the scaling of the projects we participate in and not by the quantity of them. That's the
+              cornerstone of our business model. We invest more in relationships and less in google ads.
+            </p>
+            <ButtonWithIcon
+              className="text-black shadow-black hover:bg-black hover:text-white"
+              icon={icon}
+              iconClassName="border-black after:bg-black"
+            >
+              Who we are
+            </ButtonWithIcon>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
